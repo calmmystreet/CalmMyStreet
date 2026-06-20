6f4f9b72dca0f933eefac6df797e8c8b345274a0
Avery <calmmystreet@gmail.com>
2026-06-19 18:27:30 -0700

    Change title text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <article id="welcome" class="max-w-prose mx-auto prose-base lg:prose-xl text-white">
-	<h1 class="text-pretty">Our streets are for us</h1>
-	<h2>Tell us what you want.</h2>
+	<h1 class="text-pretty">Calm My Street</h1>
+	<h2>Report where you feel unsafe on our streets</h2>
 	<p>
 		We're creating a map of where people feel unsafe.
 		<b>Drop a pin and tell your story of drivers make you feel unsafe on our streets.</b>
